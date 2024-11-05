372c2eba5cb0a7399d2f6db0f8e85d3b4d84e344
Lain Iwakura <37397127+Chamstin@users.noreply.github.com>
2024-11-05 15:33:57 +0800

    Update config.ts

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'vitepress';
 // refer https://vitepress.dev/reference/site-config for details
 export default defineConfig({
   lang: 'en-US',
+   base: '/cyberia/',
   title: '岩倉玲音の小屋',
   description: 'Welcome to CYBERIA.',
 
